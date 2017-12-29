b8f9f6e02367c051741711331650893cd1a41fda
Marco Vanali <Marco91Vanali@gmail.com>
2017-12-29 13:41:53 +0000

    Created BowlingGame class which is going to manage, let comunicate the bowlingball class and bowlingframe class. Created test for the BowlingFrame class to test the method saveScore, which save a given score in its scores array.
    Incapsulated bowlingball and bowlingframe class inside the BowlingGame class. It contain a frames array which will keep all the frame of the game, and a turn variable which will keep count of the turnation.
    sketched the rollAndSave method in the BowlingGame class, which will take care of roll the ball and save the score in the correct frame.
    
    All test green.

---
+++ spec/bowlingFrameSpec.js
@@ -0,0 +1,14 @@
+describe('BowlingFrame', function() {
+
+  beforeEach(function() {
+     bowlingframe = new BowlingFrame()
+  });
+
+  describe('saveScore', function() {
+    it('Rakes a score and save it in the scores array', function () {
+      bowlingframe.saveScore(10)
+      expect(bowlingframe.scores[0]).toEqual(10)
+    });
+  });
+
+});
